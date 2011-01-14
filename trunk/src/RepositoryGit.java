93d23ac6e1c8042295c0f343a4a8e2eaea6b098a
torsten <torsten@61ac04ec-7f51-4014-a5e0-fa667590ed9b>
2011-01-14 00:12:24 +0000

    Java source

---
+++ trunk/src/RepositoryGit.java
@@ -0,0 +1,139 @@
+/***********************************************************************\
+*
+* $Source: /tmp/cvs/onzen/src/RepositoryGit.java,v $
+* $Revision: 1.1 $
+* $Author: torsten $
+* Contents: repository
+* Systems: all
+*
+\***********************************************************************/
+
+/****************************** Imports ********************************/
+// base
+//import java.io.ByteArrayInputStream;
+//import java.io.ByteArrayOutputStream;
+import java.io.File;
+//import java.io.FileReader;
+//import java.io.BufferedReader;
+import java.io.IOException;
+//import java.io.ObjectInputStream;
+//import java.io.ObjectOutputStream;
+//import java.io.Serializable;
+
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Comparator;
+//import java.util.Date;
+//import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+//import java.util.LinkedHashSet;
+//import java.util.ListIterator;
+//import java.util.StringTokenizer;
+
+
+/****************************** Classes ********************************/
+
+/** Git repository
+ */
+class RepositoryGit extends Repository
+{
+  // --------------------------- constants --------------------------------
+
+  // --------------------------- variables --------------------------------
+
+  // ------------------------ native functions ----------------------------
+
+  // ---------------------------- methods ---------------------------------
+
+  /** create repository
+   * @param rootPath root path
+   */
+  RepositoryGit(String rootPath)
+  {
+    super(rootPath);
+  }
+
+  /** 
+   * @param 
+   * @return 
+   */
+  public void updateStates(HashSet<FileData> fileDataHashSet, HashSet<String> fileDirectoryHashSet, boolean addNewFlag)
+  {
+  }
+
+  /** 
+   * @param 
+   * @return 
+   */
+  public void update(HashSet<FileData> fileDataHashSet)
+  {
+  }
+
+  /** 
+   * @param 
+   * @return 
+   */
+  public void commit(HashSet<FileData> fileDataHashSet, Message commitMessage)
+    throws RepositoryException
+  {
+  }
+
+  /** 
+   * @param 
+   * @return 
+   */
+  public void add(HashSet<FileData> fileDataHashSet, Message commitMessage, boolean binaryFlag)
+    throws RepositoryException
+  {
+  }
+
+  /** 
+   * @param 
+   * @return 
+   */
+  public void remove(HashSet<FileData> fileDataHashSet, Message commitMessage)
+    throws RepositoryException
+  {
+  }
+
+  /** 
+   * @param 
+   * @return 
+   */
+  public void revert(HashSet<FileData> fileDataHashSet)
+    throws RepositoryException
+  {
+  }
+
+  /** 
+   * @param 
+   * @return 
+   */
+  public void diff(FileData fileData)
+    throws RepositoryException
+  {
+  }
+
+  /** 
+   * @param 
+   * @return 
+   */
+  public void patch(FileData fileData)
+    throws RepositoryException
+  {
+  }
+
+  /** convert data to string
+   * @return string
+   */
+  public String toString()
+  {
+    return "{Repository "+rootPath+"}";
+  }
+
+  //-----------------------------------------------------------------------
+
+}
+
+/* end of file */
