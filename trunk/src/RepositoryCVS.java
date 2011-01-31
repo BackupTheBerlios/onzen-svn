e27a6064d46c6ec8b42183945edcf44d9747d379
torsten <torsten@61ac04ec-7f51-4014-a5e0-fa667590ed9b>
2011-01-31 02:49:01 +0000

    - added get annotations - fixed diff

--- trunk/src/RepositoryCVS.java
+++ trunk/src/RepositoryCVS.java
@@ -1,7 +1,7 @@
 /***********************************************************************\
 *
 * $Source: /tmp/cvs/onzen/src/RepositoryCVS.java,v $
-* $Revision: 1.3 $
+* $Revision: 1.4 $
 * $Author: torsten $
 * Contents: repository
 * Systems: all
@@ -382,7 +382,6 @@ Dprintf.dprintf("file=%s",fileData);
    * @return 
    */
   public String getLastRevision()
-    throws RepositoryException
   {
     return "HEAD";
   }
@@ -646,11 +645,13 @@ Dprintf.dprintf("file=%s",fileData);
           fileDataSet.add(new FileData(matcher.group(1),FileData.States.REMOVED));
           mergeFlag = false;
         }
-        else if ((matcher = PATTERN_EMPTY.matcher(line)).matches())
+        else if (PATTERN_EMPTY.matcher(line).matches())
         {
+          // skip empty lines
         }
         else
         {
+          // unknown line
 Dprintf.dprintf("unknown %s",line);
         }
       }
@@ -927,6 +928,239 @@ Dprintf.dprintf("unknown %s",line);
       Matcher matcher;
       String  line;
 
+      String[] newFileLines = null;
+      if (newRevision != null)
+      {
+        // check out new revision
+        command.clear();
+        command.append("cvs","up","-p");
+        command.append("-r",newRevision);
+        command.append("--");
+        command.append(fileData.getFileName());
+        exec = new Exec(rootPath,command);
+
+        // read content
+        ArrayList<String> newFileLineList = new ArrayList<String>();
+        while ((line = exec.getStdout()) != null)
+        {
+          newFileLineList.add(line);
+        }
+
+        // done
+        exec.done();
+
+        newFileLines = newFileLineList.toArray(new String[newFileLineList.size()]);
+      }
+      else
+      {
+        // use local revision
+        try
+        {
+          // open file
+          BufferedReader bufferedReader = new BufferedReader(new FileReader(fileData.getFileName(rootPath)));
+
+          // read content
+          ArrayList<String> newFileLineList = new ArrayList<String>();
+          while ((line = bufferedReader.readLine()) != null)
+          {
+            newFileLineList.add(line);
+          }
+
+          // close file
+          bufferedReader.close();
+
+          newFileLines = newFileLineList.toArray(new String[newFileLineList.size()]);
+        }
+        catch (IOException exception)
+        {
+          throw new RepositoryException(exception);
+        }
+      }
+
+      // diff
+      command.clear();
+      command.append("cvs","diff");
+      if (oldRevision != null) command.append("-r",oldRevision);
+      if (newRevision != null) command.append("-r",newRevision);
+      command.append("--");
+      command.append(fileData.getFileName());
+      exec = new Exec(rootPath,command);
+
+      // skip diff header
+      while ((line = exec.getStdout()) != null)
+      {
+        if (PATTERN_DIFF.matcher(line).matches()) break;
+      }
+
+      /* parse diff output
+           Format:
+             <i>a<j> - lines added 
+             <i>d<j> - lines delete 
+             <i>c<j> - lines changed 
+      */
+      int                lineNb = 1;
+      DiffData           diffData;
+      ArrayList<String> keepLinesList = new ArrayList<String>();
+      ArrayList<String> addedLinesList = new ArrayList<String>();
+      ArrayList<String> deletedLinesList = new ArrayList<String>();
+      while ((line = exec.getStdout()) != null)
+      {
+//Dprintf.dprintf("line=%s",line);
+        if      ((matcher = PATTERN_DIFF_ADD.matcher(line)).matches())
+        {
+          // add lines
+          int[] oldIndex = parseDiffIndex(matcher.group(1));
+          int[] newIndex = parseDiffIndex(matcher.group(2));
+//Dprintf.dprintf("oldIndex=%d,%d",oldIndex[0],oldIndex[1]);
+//Dprintf.dprintf("newIndex=%d,%d",newIndex[0],newIndex[1]);
+
+          // get keep lines
+          keepLinesList.clear();
+          while ((lineNb < newIndex[0]) && (lineNb <= newFileLines.length))
+          {
+            keepLinesList.add(newFileLines[lineNb-1]);
+            lineNb++;
+          }
+          diffData = new DiffData(DiffData.BlockTypes.KEEP,keepLinesList);
+          diffDataList.add(diffData);
+//Dprintf.dprintf("diffData=%s",diffData);
+
+          // get added lines
+          addedLinesList.clear();
+          for (int z = 0; z < newIndex[1]; z++)
+          {
+            line = exec.getStdout();
+            if (!line.startsWith(">")) throw new RepositoryException("Invalid add diff output: '"+line+"'");
+            addedLinesList.add(line.substring(2));
+            lineNb++;
+          }
+          diffData = new DiffData(DiffData.BlockTypes.ADD,addedLinesList);
+          diffDataList.add(diffData);
+//Dprintf.dprintf("diffData=%s",diffData);
+        }        
+        else if ((matcher = PATTERN_DIFF_DELETE.matcher(line)).matches())
+        {
+          // delete lines
+          int[] oldIndex = parseDiffIndex(matcher.group(1));
+          int[] newIndex = parseDiffIndex(matcher.group(2));
+
+          // get keep lines
+          keepLinesList.clear();
+          while ((lineNb <= newIndex[0]) && (lineNb <= newFileLines.length))
+          {
+            keepLinesList.add(newFileLines[lineNb-1]);
+            lineNb++;
+          }
+          diffData = new DiffData(DiffData.BlockTypes.KEEP,keepLinesList);
+          diffDataList.add(diffData);
+//Dprintf.dprintf("diffData=%s",diffData);
+
+          // get deleted lines
+          deletedLinesList.clear();
+          for (int z = 0; z < oldIndex[1]; z++)
+          {
+            line = exec.getStdout();
+            if (!line.startsWith("<")) throw new RepositoryException("Invalid delete diff output: '"+line+"'");
+            deletedLinesList.add(line.substring(2));
+          }
+          diffData = new DiffData(DiffData.BlockTypes.DELETE,deletedLinesList);
+          diffDataList.add(diffData);
+//Dprintf.dprintf("diffData=%s",diffData);
+        }        
+        else if ((matcher = PATTERN_DIFF_CHANGE.matcher(line)).matches())
+        {
+          // change lines
+          int[] oldIndex = parseDiffIndex(matcher.group(1));
+          int[] newIndex = parseDiffIndex(matcher.group(2));
+//Dprintf.dprintf("oldIndex=%d,%d",oldIndex[0],oldIndex[1]);
+//Dprintf.dprintf("newIndex=%d,%d",newIndex[0],newIndex[1]);
+
+          // get keep lines
+          keepLinesList.clear();
+          while ((lineNb < newIndex[0]) && (lineNb <= newFileLines.length))
+          {
+            keepLinesList.add(newFileLines[lineNb-1]);
+            lineNb++;
+          }
+          diffData = new DiffData(DiffData.BlockTypes.KEEP,keepLinesList);
+          diffDataList.add(diffData);
+//Dprintf.dprintf("diffData=%s",diffData);
+
+          // get delete lines
+          deletedLinesList.clear();
+          for (int z = 0; z < oldIndex[1]; z++)
+          {
+            line = exec.getStdout();
+            if (!line.startsWith("<")) throw new RepositoryException("Invalid change diff output: '"+line+"'");
+            deletedLinesList.add(line.substring(2));
+          }
+
+          // skip separator "---"
+          line = exec.getStdout();
+          if (!line.startsWith("---")) throw new RepositoryException("Invalid diff output: expected separator, got '"+line+"'");
+
+          // get added lines
+          addedLinesList.clear();
+          for (int z = 0; z < newIndex[1]; z++)
+          {
+            line = exec.getStdout();
+            if (!line.startsWith(">")) throw new RepositoryException("Invalid change diff output: '"+line+"'");
+            addedLinesList.add(line.substring(2));
+            lineNb++;
+          }
+
+          diffData = new DiffData(DiffData.BlockTypes.CHANGE,addedLinesList,deletedLinesList);
+          diffDataList.add(diffData);
+//Dprintf.dprintf("diffData=%s",diffData);
+        }
+else {
+//Dprintf.dprintf("line=%s",line);
+}
+      }
+
+      // get rest of keep lines
+      if (lineNb <= newFileLines.length)
+      {
+        keepLinesList.clear();
+        while (lineNb <= newFileLines.length)
+        {
+          keepLinesList.add(newFileLines[lineNb-1]);
+          lineNb++;
+        }
+        diffData = new DiffData(DiffData.BlockTypes.KEEP,keepLinesList);
+        diffDataList.add(diffData);
+      }
+//Dprintf.dprintf("diffData=%s",diffData);
+    }
+    catch (IOException exception)
+    {
+      throw new RepositoryException(exception);
+    }
+
+    return diffDataList.toArray(new DiffData[diffDataList.size()]);
+  }
+
+  /** 
+   * @param 
+   * @return 
+   */
+  public DiffData[] diffOLD(FileData fileData, String oldRevision, String newRevision)
+    throws RepositoryException
+  {
+    final Pattern PATTERN_DIFF        = Pattern.compile("^diff.*",Pattern.CASE_INSENSITIVE);
+    final Pattern PATTERN_DIFF_ADD    = Pattern.compile("^([\\d,]+)a([\\d,]+)",Pattern.CASE_INSENSITIVE);
+    final Pattern PATTERN_DIFF_DELETE = Pattern.compile("^([\\d,]+)d([\\d,]+)",Pattern.CASE_INSENSITIVE);
+    final Pattern PATTERN_DIFF_CHANGE = Pattern.compile("^([\\d,]+)c([\\d,]+)",Pattern.CASE_INSENSITIVE);
+
+    ArrayList<DiffData> diffDataList = new ArrayList<DiffData>();
+
+    try
+    {
+      Command command = new Command();
+      Exec    exec;
+      Matcher matcher;
+      String  line;
+
       String[] oldFileLines = null;
       if (oldRevision != null)
       {
@@ -1146,6 +1380,67 @@ else {
   {
   }
 
+  /** get annotations to file
+   * @param fileData file data
+   * @param revision revision
+   * @return annotation array
+   */
+  public AnnotationData[] annotations(FileData fileData, String revision)
+    throws RepositoryException
+  {
+    final Pattern PATTERN_ANNOTATION = Pattern.compile("^\\s*([\\.\\d]+)\\s+\\((\\S+)\\s+(\\S+)\\):\\s(.*)\\s*",Pattern.CASE_INSENSITIVE);
+    final Pattern PATTERN_EMPTY      = Pattern.compile("^\\s*",Pattern.CASE_INSENSITIVE);
+
+    ArrayList<AnnotationData> annotationDataList = new ArrayList<AnnotationData>();
+
+    try
+    {
+      Command command = new Command();
+      Exec    exec;
+      Matcher matcher;
+      String  line;
+
+      // get annotations
+      command.clear();
+      command.append("cvs","annotate");
+      if (revision != null) command.append("-r",revision);
+      command.append("--");
+      command.append(fileData.getFileName());
+      exec = new Exec(rootPath,command);
+
+      /* parse annotation output
+           Format:
+             <revision> <author> <date> <line>
+      */
+      while ((line = exec.getStdout()) != null)
+      {
+        if      ((matcher = PATTERN_ANNOTATION.matcher(line)).matches())
+        {
+          annotationDataList.add(new AnnotationData(matcher.group(1),
+                                                    matcher.group(2),
+                                                    parseDate(matcher.group(3)),
+                                                    matcher.group(4))
+                                                   );
+        }
+        else if (PATTERN_EMPTY.matcher(line).matches())
+        {
+          // skip empty lines
+        }
+        else
+        {
+          // unknown line
+Dprintf.dprintf("unknown %s",line);
+        }
+      }
+    }
+    catch (IOException exception)
+    {
+      throw new RepositoryException(exception);
+    }
+
+    return annotationDataList.toArray(new AnnotationData[annotationDataList.size()]);
+  }
+
   /** convert data to string
    * @return string
    */
@@ -1281,17 +1576,7 @@ else {
           }
           else if ((matcher = PATTERN_DATE_AUTHOR.matcher(line)).matches())
           {
-            try
-            {
-              date = new SimpleDateFormat().parse(matcher.group(1));
-//              date = DateFormat.getDateInstance().parse(matcher.group(1));
-Dprintf.dprintf("date=%s",date);
-            }
-            catch (ParseException exception)
-            {
-Dprintf.dprintf("exception=%s",exception);
-              date = new Date();
-            }
+            date   = parseDate(matcher.group(1));
             author = matcher.group(2);
 
             // get branch ids
@@ -1374,7 +1659,7 @@ Dprintf.dprintf("exception=%s",exception);
   {
     assert numbers.length+delta >= 0 : numbers.length+", "+delta;
 
-    StringBuffer buffer = new StringBuffer();
+    StringBuilder buffer = new StringBuilder();
     for (int z = 0; z < numbers.length+delta; z++)
     {
       if (buffer.length() > 0) buffer.append('.');
