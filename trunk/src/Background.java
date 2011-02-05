9fdf7852864cdef126c7397acd29abf480b9fd06
torsten <torsten@61ac04ec-7f51-4014-a5e0-fa667590ed9b>
2011-02-05 09:07:12 +0000

    added specific run-method

--- trunk/src/Background.java
+++ trunk/src/Background.java
@@ -1,7 +1,7 @@
 /***********************************************************************\
 *
 * $Source: /tmp/cvs/onzen/src/Background.java,v $
-* $Revision: 1.1 $
+* $Revision: 1.2 $
 * $Author: torsten $
 * Contents: background task
 * Systems: all
@@ -9,6 +9,8 @@
 \***********************************************************************/
 
 /****************************** Imports ********************************/
+
+import java.lang.reflect.Method;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
@@ -24,7 +26,9 @@ abstract class BackgroundRunnable implements Runnable
   // --------------------------- constants --------------------------------
 
   // --------------------------- variables --------------------------------
-  protected Object[] userData;
+  protected Object[]            userData;
+
+  private   StackTraceElement[] stackTrace;
 
   // ------------------------ native functions ----------------------------
 
@@ -36,11 +40,108 @@ abstract class BackgroundRunnable implements Runnable
   BackgroundRunnable(Object... userData)
   {
     this.userData = userData;
+    this.stackTrace         = Thread.currentThread().getStackTrace();
   }
 
   /** run method
    */
-  abstract public void run();
+  public void run()
+  {
+    Onzen.printInternalError("No run method declared for background task!");
+    System.err.println("Stack trace:");
+    for (int z = 2; z < stackTrace.length; z++)
+    {
+      System.err.println("  "+stackTrace[z]);
+    }
+    System.exit(Onzen.EXITCODE_INTERNAL_ERROR);
+  }
+}
+
+/** background task
+ */
+class BackgroundTask implements Runnable
+{
+  // --------------------------- constants --------------------------------
+
+  // --------------------------- variables --------------------------------
+  private BackgroundRunnable backgroundRunnable;
+  private Method             runMethod;
+
+  // ------------------------ native functions ----------------------------
+
+  // ---------------------------- methods ---------------------------------
+
+  /** create background task
+   * @param backgroundRunnable background runnable to execute
+   */
+  BackgroundTask(BackgroundRunnable backgroundRunnable)
+  {
+    this.backgroundRunnable = backgroundRunnable;
+    this.runMethod          = null;
+
+    // find match run-method if possible
+    for (Method method : backgroundRunnable.getClass().getDeclaredMethods())
+    {
+      // check name
+      if (method.getName().equals("run"))
+      {
+        // check parameters
+        Class[] parameterTypes = method.getParameterTypes();
+        if (parameterTypes.length == backgroundRunnable.userData.length)
+        {
+          boolean match = true;
+          for (int z = 0; (z < backgroundRunnable.userData.length) && match; z++)
+          {
+            if (backgroundRunnable.userData[z] != null)
+            {
+              match = parameterTypes[z].isAssignableFrom(backgroundRunnable.userData[z].getClass());
+            }
+            else
+            {
+              match = Object.class.isAssignableFrom(parameterTypes[z]);
+            }
+          }
+
+          if (match)
+          {
+            runMethod = method;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  /** run background runnable
+   */
+  public void run()
+  {
+    try
+    {
+      if (runMethod != null)
+      {
+        // call specific run-method
+        runMethod.invoke(backgroundRunnable,backgroundRunnable.userData);
+      }
+      else
+      {
+        // call general run-method
+        backgroundRunnable.run();
+      }
+    }
+    catch (Exception exception)
+    {
+      Onzen.printError("Unhandled background exception: %s",exception);
+      System.err.println("Stack trace:");
+      exception.printStackTrace();
+    }
+    catch (Error exception)
+    {
+      Onzen.printError("Unhandled background error: %s",exception);
+      System.err.println("Stack trace:");
+      exception.printStackTrace();
+    }
+  }
 }
 
 /** background executor
@@ -71,7 +172,7 @@ public class Background
    */
   public static void run(BackgroundRunnable backgroundRunnable)
   {
-    executorService.submit(backgroundRunnable);
+    executorService.submit(new BackgroundTask(backgroundRunnable));
   }
 }
 
