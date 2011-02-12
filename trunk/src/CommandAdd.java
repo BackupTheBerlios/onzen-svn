29efba7cac0d5ae4bc3f624711717d51a4f0fa4b
torsten <torsten@61ac04ec-7f51-4014-a5e0-fa667590ed9b>
2011-02-12 08:35:59 +0000

    clean-up file data set variable

--- trunk/src/CommandAdd.java
+++ trunk/src/CommandAdd.java
@@ -1,7 +1,7 @@
 /***********************************************************************\
 *
 * $Source: /tmp/cvs/onzen/src/CommandAdd.java,v $
-* $Revision: 1.9 $
+* $Revision: 1.10 $
 * $Author: torsten $
 * Contents: command add files/directories
 * Systems: all
@@ -99,14 +99,12 @@ class CommandAdd
    */
   class Data
   {
-    HashSet<FileData> fileDataSet;
-    String            message;
-    boolean           immediateCommitFlag;
-    boolean           binaryFlag;
+    String  message;
+    boolean immediateCommitFlag;
+    boolean binaryFlag;
 
     Data()
     {
-      this.fileDataSet         = new HashSet<FileData>();
       this.message             = "";
       this.immediateCommitFlag = false;
       this.binaryFlag          = false;
@@ -115,37 +113,37 @@ class CommandAdd
 
   // --------------------------- constants --------------------------------
 
-  // colors
-
   // --------------------------- variables --------------------------------
 
   // global variable references
-  private final RepositoryTab repositoryTab;
-  private final Display       display;
+  private final RepositoryTab     repositoryTab;
+  private final HashSet<FileData> fileDataSet;
+  private final Shell             shell;
+  private final Display           display;
+  private final String[]          history;       
 
   // dialog
-  private final Data          data = new Data();
-  private final Shell         dialog;        
-  private final String[]      history;       
+  private final Data              data = new Data();
+  private final Shell             dialog;        
 
   // widgets
-  private final List          widgetFiles;   
-  private final List          widgetHistory; 
-  private final Text          widgetMessage; 
-  private final Button        widgetImmediateCommit;  
-  private final Button        widgetBinary;  
-  private final Button        widgetAdd;     
+  private final List              widgetFiles;   
+  private final List              widgetHistory; 
+  private final Text              widgetMessage; 
+  private final Button            widgetImmediateCommit;  
+  private final Button            widgetBinary;  
+  private final Button            widgetAdd;     
 
   // ------------------------ native functions ----------------------------
 
   // ---------------------------- methods ---------------------------------
 
   /** add command
-   * @param repositoryTab repository tab
    * @param shell shell
-   * @param repository repository
+   * @param repositoryTab repository tab
+   * @param fileDataSet files to add
    */
-  CommandAdd(RepositoryTab repositoryTab, final Shell shell, final Repository repository)
+  CommandAdd(final Shell shell, final RepositoryTab repositoryTab, HashSet<FileData> fileDataSet)
   {
     Composite composite,subComposite;
     Label     label;
@@ -153,6 +151,8 @@ class CommandAdd
 
     // initialize variables
     this.repositoryTab = repositoryTab;
+    this.fileDataSet   = fileDataSet;
+    this.shell         = shell;
 
     // get display
     display = shell.getDisplay();
@@ -161,7 +161,7 @@ class CommandAdd
     history = Message.getHistory();
 
     // add files dialog
-    dialog = Dialogs.open(shell,"Add files",Settings.geometryAdd.x,Settings.geometryAdd.y,new double[]{1.0,0.0},1.0);
+    dialog = Dialogs.open(shell,"Add files",new double[]{1.0,0.0},1.0);
 
     composite = Widgets.newComposite(dialog);
     composite.setLayout(new TableLayout(new double[]{0.0,1.0,0.0,1.0,0.0,1.0},1.0,4));
@@ -199,12 +199,16 @@ class CommandAdd
       widgetMessage.setToolTipText("Commit message.\n\nUse Ctrl-Up/Down/Home/End to select message from history.\n\nUse Ctrl-Return to add files.");
 
       subComposite = Widgets.newComposite(composite);
-      subComposite.setLayout(new TableLayout(null,new double[]{0.0,1.0}));
+      subComposite.setLayout(new TableLayout(null,1.0));
       Widgets.layout(subComposite,6,0,TableLayoutData.WE);
       {
+        widgetBinary = Widgets.newCheckbox(subComposite,"binary");
+        Widgets.layout(widgetBinary,0,0,TableLayoutData.W);
+        widgetBinary.setToolTipText("Select this checkbox to add binary files.");
+
         widgetImmediateCommit = Widgets.newCheckbox(subComposite,"immediate commit");
         widgetImmediateCommit.setSelection(Settings.immediateCommit);
-        Widgets.layout(widgetImmediateCommit,0,0,TableLayoutData.W);
+        Widgets.layout(widgetImmediateCommit,0,1,TableLayoutData.E);
         widgetImmediateCommit.addSelectionListener(new SelectionListener()
         {
           public void widgetDefaultSelected(SelectionEvent selectionEvent)
@@ -217,10 +221,6 @@ class CommandAdd
           }
         });
         widgetImmediateCommit.setToolTipText("Select this checkbox to commit added files immediately.");
-
-        widgetBinary = Widgets.newCheckbox(subComposite,"binary");
-        Widgets.layout(widgetBinary,0,1,TableLayoutData.W);
-        widgetBinary.setToolTipText("Select this checkbox to add binary files.");
       }
     }
 
@@ -230,7 +230,7 @@ class CommandAdd
     Widgets.layout(composite,1,0,TableLayoutData.WE,0,0,4);
     {
       widgetAdd = Widgets.newButton(composite,"Add");
-      Widgets.layout(widgetAdd,0,0,TableLayoutData.W,0,0,0,0,70,SWT.DEFAULT);
+      Widgets.layout(widgetAdd,0,0,TableLayoutData.W,0,0,0,0,SWT.DEFAULT,SWT.DEFAULT,70,SWT.DEFAULT);
       widgetAdd.addSelectionListener(new SelectionListener()
       {
         public void widgetDefaultSelected(SelectionEvent selectionEvent)
@@ -253,7 +253,7 @@ class CommandAdd
       widgetAdd.setToolTipText("Add files.");
 
       button = Widgets.newButton(composite,"Cancel");
-      Widgets.layout(button,0,1,TableLayoutData.E,0,0,0,0,70,SWT.DEFAULT);
+      Widgets.layout(button,0,1,TableLayoutData.E,0,0,0,0,SWT.DEFAULT,SWT.DEFAULT,70,SWT.DEFAULT);
       button.addSelectionListener(new SelectionListener()
       {
         public void widgetDefaultSelected(SelectionEvent selectionEvent)
@@ -355,7 +355,16 @@ class CommandAdd
     });
 
     // show dialog
-    Dialogs.show(dialog);
+    Dialogs.show(dialog,Settings.geometryAdd);
+
+    // add files
+    if (!widgetFiles.isDisposed())
+    {
+      for (FileData fileData : fileDataSet)
+      {
+        widgetFiles.add(fileData.getFileName());
+      }
+    }
 
     // add history
     if (!widgetHistory.isDisposed())
@@ -374,36 +383,13 @@ class CommandAdd
   }
 
   /** add command
-   * @param repositoryTab repository tab
    * @param shell shell
-   * @param repository repository
-   * @param fileDataSet files to add
-   */
-  CommandAdd(RepositoryTab repositoryTab, Shell shell, Repository repository, HashSet<FileData> fileDataSet)
-  {
-    this(repositoryTab,shell,repository);
-
-    // add files
-    for (FileData fileData : fileDataSet)
-    {
-      data.fileDataSet.add(fileData);
-      widgetFiles.add(fileData.name);
-    }
-  }
-
-  /** add command
    * @param repositoryTab repository tab
-   * @param shell shell
-   * @param repository repository
    * @param fileData file to add
    */
-  CommandAdd(RepositoryTab repositoryTab, Shell shell, Repository repository, FileData fileData)
+  CommandAdd(Shell shell, RepositoryTab repositoryTab, FileData fileData)
   {
-    this(repositoryTab,shell,repository);
-
-    // add file
-    data.fileDataSet.add(fileData);
-    widgetFiles.add(fileData.name);
+    this(shell,repositoryTab,fileData.toSet());
   }
 
   /** run dialog
@@ -460,18 +446,18 @@ class CommandAdd
     {
       // add files
       if (data.immediateCommitFlag) message = new Message(data.message);
-      repositoryTab.repository.add(data.fileDataSet,message,data.binaryFlag);
+      repositoryTab.repository.add(fileDataSet,message,data.binaryFlag);
 
       // add message to history
       message.addToHistory();
 
       // update file states
-      repositoryTab.repository.updateStates(data.fileDataSet);
+      repositoryTab.repository.updateStates(fileDataSet);
       display.syncExec(new Runnable()
       {
         public void run()
         {
-          repositoryTab.updateFileStatus(data.fileDataSet);
+          repositoryTab.updateTreeItems(fileDataSet);
         }
       });
     }
@@ -482,7 +468,7 @@ class CommandAdd
       {
         public void run()
         {
-          Dialogs.error(dialog,"Cannot add files (error: %s)",exceptionMessage);
+          Dialogs.error(shell,"Cannot add files (error: %s)",exceptionMessage);
         }
       });
       return;
