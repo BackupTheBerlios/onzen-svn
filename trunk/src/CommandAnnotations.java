8bc50a9d1f5f8ee86fe7d69c76b4a0aed44706e5
trupp <trupp@61ac04ec-7f51-4014-a5e0-fa667590ed9b>
2011-02-19 05:52:08 +0000

    cleakn-up imports

--- trunk/src/CommandAnnotations.java
+++ trunk/src/CommandAnnotations.java
@@ -15,20 +15,13 @@
 //import java.io.BufferedReader;
 //import java.io.IOException;
 
-//import java.text.SimpleDateFormat;
-
 //import java.util.ArrayList;
 //import java.util.Arrays;
-//import java.util.BitSet;
-//import java.util.Comparator;
 //import java.util.Date;
 //import java.util.HashMap;
 //import java.util.HashSet;
 //import java.util.LinkedList;
 //import java.util.LinkedHashSet;
-//import java.util.ListIterator;
-//import java.util.StringTokenizer;
-//import java.util.WeakHashMap;
 
 // graphics
 import org.eclipse.swt.custom.CaretEvent;
