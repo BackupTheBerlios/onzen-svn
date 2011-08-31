a3365a2674a2c6a24493a1722802216baa13447c
trupp <trupp@61ac04ec-7f51-4014-a5e0-fa667590ed9b>
2011-08-31 13:45:50 +0000

    find find: ignore case

--- trunk/src/CommandCreatePatch.java
+++ trunk/src/CommandCreatePatch.java
@@ -746,7 +746,7 @@ Dprintf.dprintf("");
         int cursorIndex = widgetPatch.getCaretOffset();
 
         // search
-        int offset = widgetPatch.getText().substring(cursorIndex).indexOf(findText);
+        int offset = widgetPatch.getText().toLowerCase().substring(cursorIndex).indexOf(findText);
         if (offset >= 0)
         {
           widgetPatch.redraw();
