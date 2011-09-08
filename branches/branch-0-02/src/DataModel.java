188c2d1a4d77b42af0daa12edca431dc5278c9b1
trupp <trupp@61ac04ec-7f51-4014-a5e0-fa667590ed9b>
2011-09-08 07:09:51 +0000

    obsolete

--- branches/branch-0-02/src/DataModel.java
+++
@@ -1,9 +0,0 @@
-class DataModel
-{
-  public static void main(String[] args)
-  {
-    int dataModel = Integer.parseInt(System.getProperty("sun.arch.data.model"));
-    System.out.println(dataModel);
-    System.exit(dataModel);
-  }
-}
