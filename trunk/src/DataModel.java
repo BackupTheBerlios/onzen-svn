9f7271f909d1bc93e42d78be63fae1caa0dfa6c3
trupp <trupp@61ac04ec-7f51-4014-a5e0-fa667590ed9b>
2014-01-26 07:47:20 +0000

    fixed detection of data model if property sun.arch.data.model does nto exists

--- trunk/src/DataModel.java
+++ trunk/src/DataModel.java
@@ -1,8 +1,37 @@
+import java.util.Properties;
+import java.util.Enumeration;
+
 class DataModel
 {
   public static void main(String[] args)
   {
-    int dataModel = Integer.parseInt(System.getProperty("sun.arch.data.model"));
+    /*
+    Properties  properties = java.lang.System.getProperties();
+    Enumeration names = properties.propertyNames();
+    while (names.hasMoreElements())
+    {
+      String name = (String)names.nextElement();
+      String value = properties.getProperty(name);
+      System.out.println(name + " := " + value);
+    }
+    */
+
+    int dataModel = 32;
+
+    String value;
+
+    value = System.getProperty("sun.arch.data.model");
+    if (value != null)
+    {
+      dataModel = Integer.parseInt(value);
+    }
+
+    value = System.getProperty("os.arch");
+    if (value != null)
+    {
+      dataModel = value.contains("64") ? 64 : 32;
+    }
+
     System.out.println(dataModel);
     System.exit(dataModel);
   }
